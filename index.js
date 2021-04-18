21ebc04704e8dd30eea9730d6d8d0766d8e0bba5
manuel duron <manniemd@gmail.com>
2021-04-18 16:21:32 -0400

    the working code

--- index.js
+++ index.js
@@ -1,13 +1,158 @@
+const fs = require('fs');
+const inquierer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+// const utils = require('utils');
+const inquirer = require('inquirer');
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'Whats repo name? (Required)',
+    //validate to see value
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+         } else {
+        console.log('enter repo title.');
+        return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Give a short description of repo. (Required)',
+    //validate to see value
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Give a short description of repo.');
+            return false;
+        }
+    }
+    },
+//instalation confirmation 
+{
+    type: 'confirm',
+    name: 'confirmInstallation',
+    message: 'Is there an installation process?'
+    },
+{
+    type: 'input',
+    name: 'Instalation',
+    message: 'List the installation process instructions.',
+    when: ({ confirmInstallation }) => {
+        if (confirmInstallation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
+
+{ //confirm
+    type: 'confirm',
+    name: 'confirmUsage',
+    message: 'Would you like to add instruction of how to use app?'
+},
+{
+    type: 'input',
+    name: 'Usage',
+    message: 'List instructions for app. use images if possible.',
+    when: ({ confirmUsage }) => {
+        if (confirmUsage) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
+
+{ //confirm
+    type: 'confirm',
+    name: 'confirmContribution',
+    message: 'Can other developers contribute to repo?'
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'list hop developers can contribute to project.',
+    when: ({ confirmContribution }) => {
+        if (confirmContribution) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
+
+{
+    type: 'checkbox',
+    name: 'licenses',
+    message: 'chose a license, you can also choose none and add one later.',
+    choices: ['none', 'GNU AGPLv3', 'MIT', 'ISC', 'GNU GPL v2', 
+    'Apache License 2.0', 'Mozilla Public License 2.0', 'The Unlicense']
+},
+
+{
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username? (Requiered)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a valid GitHub Username.');
+            return false;
+        }
+    }
+},
+
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter email address  (Requiered)',
+    validate: nameInput => {
+       if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a valid email.');
+            return false;
+        }
+    }
+    
+    
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileContent, error => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const userAnswers = await inquirer.prompt(questions);
+        console.log('datta is being processed into README.MD:', userAnswers);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const generateBadges = licensesArr => {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  const badgesMdArr = licensesArr.map((license) => {
+    if (license === "MIT") {
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)`;
+    } else if (license === 'ISC') {
+      return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)(https://opensource.org/licenses/ISC)`;
+    } else if (license === 'GNU GPL v2') {
+      return `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`;
+    } else {
+      return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)`;
+    }
+  });
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  return badgesMdArr.join(' ');
+};
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function to generate markdown for README
+const generateMarkdown = data => {
 
-`;
+  const {
+    title,
+    description,
+    installation,
+    usage,
+    contributing,
+    Licenses,
+    github,
+    email
+  } = data;
+
+  return `
+  # ${title}
+  ${generateBadges(licenses)}
+  ## Description
+  ${description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+
+  
+  ## Installation
+  ${installation}
+  ## Usage
+  ${usage}
+  ## License(s)
+  ${licenses.join(', ')}
+  ## Contributing
+  ${contributing}
+  find me on Github [${github}]()
+  email me: ${email}
+  `;
 }
 
 module.exports = generateMarkdown;
